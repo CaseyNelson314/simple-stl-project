f64c5d0bb30e9fbec739486bc6cb46ad0d9752ac
CaseyNelson314 <caseynelson314@gmail.com>
2022-12-24 23:44:08 +0900

    functional追加

---
+++ functional/functional.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <algorithm>  // std::forward
+
+template <class> class function;
+
+template <class R, class... Args>
+class function<R(Args...)> {
+
+	// 関数を保持するための仮想関数テーブル
+	struct Vtable {
+		R(*call)(void* function_ptr, Args&&... args);
+		void (*destroy)(void* function_ptr);
+	};
+
+	Vtable* vTable;
+
+	void* functionPtr;
+
+public:
+
+	function() noexcept
+		: functionPtr(nullptr)
+		, vTable(nullptr)
+	{}
+
+	// 関数ポインタを保持するコンストラクタ
+	template <class Functor>
+	function(Functor&& f) {
+
+		// ファンクタを保持するための仮想関数テーブル
+		static Vtable staticVTable = {
+			[](void* function_ptr, Args&&... args) -> R {
+				return (*reinterpret_cast<Functor*>(function_ptr))(std::forward<Args>(args)...);
+			},
+			[](void* function_ptr) { delete reinterpret_cast<Functor*>(function_ptr); }
+		};
+
+		// 仮想関数テーブルにセット
+		functionPtr = new Functor(f);
+		vTable = &staticVTable;
+
+	}
+
+	~function() {
+
+		vTable->destroy(functionPtr);
+
+	}
+
+	R operator()(Args&&... args) {
+
+		return vTable->call(functionPtr, std::forward<Args>(args)...);
+
+	}
+
+	template <class T>
+	function(const function<T>&) = delete;
+
+	template <class T>
+	function(function<T>&&) = delete;
+
+};
---
+++ functional/main.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+#include "functional.hpp"
+
+int main() {
+
+	{
+		/// 最小構成ラムダ式
+		function<void()> f = []() {
+			std::cout << "a";
+		};
+		f();
+
+		std::cout << std::endl;
+	}
+
+	{
+		/// キャプチャ有りラムダ式
+
+		int i = 1234;
+		function<void()> f = [&]() {
+			std::cout << i;
+		};
+		f();
+
+		std::cout << std::endl;
+	}
+
+	{
+		/// 引数ありラムダ式
+		function<void(int)> f = [](int value) {
+			std::cout << value;
+		};
+		f(100);
+
+		std::cout << std::endl;
+	}
+
+
+	{
+		/// 戻り値ありラムダ式
+		function<int()> f = [&]() {
+			return 200;
+		};
+		std::cout << f();
+
+		std::cout << std::endl;
+	}
+}
